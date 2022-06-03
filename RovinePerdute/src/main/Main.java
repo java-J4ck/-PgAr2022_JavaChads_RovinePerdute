adc69bdd4eb95b2667828c70bd7f291214775eb4
DavyPintus <davyleop@gmail.com>
2022-06-03 17:17:04 +0200

    XMLWriter

--- RovinePerdute/src/main/Main.java
+++ RovinePerdute/src/main/Main.java
@@ -3,26 +3,31 @@ package main;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.LinkedList;
-import tree.*;
-import xmlutilities.*;
 
+import tree.BuildTree;
+import tree.PathFinder;
+import xmlutilities.XMLWriter;
 
 
 public class Main {
 
+	
+
 	public static void main(String[] args) {
 		
-		ArrayList<City> c = ReadCities.readCities(new File("rovine_perdute_maps//test_file//PgAr_Map_12.xml")); 
-	    BuildTree.buildTree(c, true);
-		//LinkedList<Node> path = PathFinder.dijkstra(tree);
-		
-		/*
-		for(Node n : path) {
-			System.out.println(n.getCurrentCity().getName());
-		}*/
-		
+		ArrayList<City> c = xmlutilities.ReadCities.readCities(new File("rovine_perdute_maps//test_file//PgAr_Map_5.xml")); 
+		LinkedList<City> tree_T = BuildTree.buildTree(c, true);
+		LinkedList<City> tree_M = BuildTree.buildTree(c, false);
+			
+			PathFinder
+			
+			
+			XMLWriter.XmlWrite(PathFinder.dijkstra(tree_T), PathFinder.dijkstra(tree_M));
+			
+			
+		}
 		
 		
 	}
 
-}
+
--- RovinePerdute/src/xmlutilities/XMLWriter.java
+++ RovinePerdute/src/xmlutilities/XMLWriter.java
@@ -1,5 +1,90 @@
 package xmlutilities;
 
+import java.io.FileOutputStream;
+import java.nio.file.Path;
+import java.util.LinkedList;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamWriter;
+import main.City;
+import tree.PathFinder;
+
+
+
+
 public class XMLWriter {
+
+	public static void XmlWrite(LinkedList<City>  path1, LinkedList<City> path2) {
+		
+		
+		
+
+    	XMLOutputFactory xmlof = null;
+    	XMLStreamWriter xmlw = null;
+    	try {
+	
+			 xmlof = XMLOutputFactory.newInstance();
+			 xmlw = xmlof.createXMLStreamWriter(new FileOutputStream("Routes.xml"), "utf-8");
+		//	 xmlw.writeStartDocument("utf-8", "1.0");
+			 
+			 
+			 try { // blocco try per raccogliere eccezioni
+				//############################STAMPA PERSONE#############################################################
+				  xmlw.writeStartElement("routes"); // scrittura del tag radice <programmaArnaldo>
+				  xmlw.writeStartElement("route");//apri output				  
+				  xmlw.writeAttribute("team","Tonathiu");//apri persone
+				  xmlw.writeAttribute("cost",String.format("%.2f", PathFinder.getPathCost()  ));//apri persone
+				  xmlw.writeAttribute("cities",String.format("%.2f", path1.size()  ));//apri persone
+				  
+				  for (City  c: path1 ) {
+					  
+					  xmlw.writeStartElement("City"); // start elemento nome
+					  xmlw.writeAttribute("id", String.format("%d", c.getId() )); //scrittura id	
+					  xmlw.writeAttribute("nome",c.getName());
+					  xmlw.writeEndElement();//chiusura persone  					
+					  
+				  }
+				  
+				
+				  
+				  
+				  
+				  xmlw.writeEndElement();//chiusura output
+				  
+				  
+				  
+				  xmlw.writeStartElement("route");//apri output				  
+				  xmlw.writeAttribute("team","Metztli");//apri persone
+				  xmlw.writeAttribute("cost",String.format("%.2f", PathFinder.getPathCost()  ));//apri persone
+				  xmlw.writeAttribute("cities",String.format("%.2f", path2.size()  ));//apri persone
+				  
+				  for (City  c: path2 ) {
+					  
+					  xmlw.writeStartElement("City"); // start elemento nome
+					  xmlw.writeAttribute("id", String.format("%d", c.getId() )); //scrittura id	
+					  xmlw.writeAttribute("nome",c.getName());
+					  xmlw.writeEndElement();//chiusura persone  					
+					  
+				  }
+				  
+				
+				  
+				  
+				  
+				  xmlw.writeEndElement();//chiusura output			  
+				  
+				  xmlw.writeEndElement(); // chiusura di </programmaArnaldo>
+			 } 
+			 catch (Exception e) { // se c’è un errore viene eseguita questa parte
+				 System.out.println("Errore nella scrittura");
+			 }
+
+		} 
+    	catch (Exception e) {
+			e.printStackTrace();
+    	}
+    	
+    	
+    		
+	}
 
 }
