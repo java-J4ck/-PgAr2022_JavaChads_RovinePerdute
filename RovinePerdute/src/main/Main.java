f89a1a422e3e54999914f34f5ae14d3d52078ac0
giacomo <giacomo.baresi.02@gmail.com>
2022-06-04 11:43:24 +0200

    ultimo aggiornamento

--- RovinePerdute/src/main/Main.java
+++ RovinePerdute/src/main/Main.java
@@ -2,28 +2,57 @@ package main;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.LinkedList;
-
-import tree.BuildTree;
 
 import xmlutilities.XMLWriter;
 
 
 public class Main {
 
-	
+	private static final String[] voci= {"5","12","50","200","2000","10000"};
 
 	
 	public static void main(String[] args) {
+		//menu di scelta della mappa
+		MyMenu choseFile = new MyMenu("scegli il tipo di mappa in base al numero di citta",voci);
+		String cityNumber;
 		
-		ArrayList<City> c = xmlutilities.ReadCities.readCities(new File("rovine_perdute_maps//test_file//PgAr_Map_12.xml")); 
+		int choice=choseFile.scegli();//menu di scelta mappa
+		
+		switch(choice) {//scelta mappa in base al numero di citta
+			case 0:
+				System.out.println("Grazie per aver usato il programma");
+				System.exit(0);
+			case 1:
+				cityNumber="5";
+				break;
+			case 2:
+				cityNumber="12";
+				break;
+			case 3:
+				cityNumber="50";
+				break;
+			case 4:
+				cityNumber="200";
+				break;
+			case 5:
+				cityNumber="2000";
+				break;
+			case 6:
+				cityNumber="10000";
+				break;
+			default: 
+				cityNumber="5";
 				
-		XMLWriter.XmlWrite(c);
-			
-			
 		}
+		//lettura dal file
+		ArrayList<City> c = xmlutilities.ReadCities.readCities(new File("rovine_perdute_maps//test_file//PgAr_Map_" + cityNumber+ ".xml")); 
+		//scrittura del file		
+		XMLWriter.XmlWrite(c);
+		System.out.println("Grazie per aver usato il programma");	
+			
+	 }
 		
 		
-	}
+}
 
 
